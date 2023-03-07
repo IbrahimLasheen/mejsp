f377d67b28f78eecd43006ecd7ded1ea87ebc3c5
IbrahimLasheen <123382132+IbrahimLasheen@users.noreply.github.com>
2023-03-07 22:09:15 +0200

    Create 2023_02_26_214100_add_whatsapp_photo_and_name_to_settings_table.php
    add whatsapp

---
+++ database/migrations/2023_02_26_214100_add_whatsapp_photo_and_name_to_settings_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddWhatsappPhotoAndNameToSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('settings', function (Blueprint $table) {
+            $table->string('whatsapp_photo')->nullable();
+            $table->string('whatsapp_name')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('settings', function (Blueprint $table) {
+            //
+        });
+    }
+}
